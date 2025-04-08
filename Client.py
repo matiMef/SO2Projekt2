ee41a4ca46752448cc2a1a142bd0f8ec97d8a946
d3vi1337 <dawidherejczak@gmail.com>
2025-04-08 23:51:49 +0200

    Dodanie funkcji odbierającej i wysyłającej wiadomości na kliencie.

--- Client.py
+++ Client.py
@@ -1,4 +1,5 @@
 import socket
+import threading
 
 HEADER = 64
 PORT = 5050
@@ -10,15 +11,34 @@ ADDR = (SERVER, PORT)
 socket_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket_client.connect(ADDR)
 
-def send(msg):
-    message = msg.encode(FORMAT)
-    msg_length = len(message)
-    send_length = str(msg_length).encode(FORMAT)
-    send_length += b' ' * (HEADER - len(send_length))
-    socket_client.send(send_length)
-    socket_client.send(message)
-    print(socket_client.recv(2048).decode(FORMAT))
+# Funkcja do wysyłania wiadomości
+def send():
+    while True:
+        msg = input()
+        message = msg.encode(FORMAT)
+        msg_length = len(message)
+        send_length = str(msg_length).encode(FORMAT)
+        send_length += b' ' * (HEADER - len(send_length))
+        socket_client.send(send_length)
+        socket_client.send(message)
+        if msg == DISCONNECT_MESSAGE:
+            break
+    socket_client.close()
 
-send("Hello!")
-send("Test message")
-send(DISCONNECT_MESSAGE)
+# Funkcja do odbierania wiadomości
+def receive():
+    while True:
+        try:
+            msg = socket_client.recv(2048).decode(FORMAT)
+            if msg:
+                print(f"\n[SERVER]: {msg}")
+        except:
+            print("[ERROR] Connection closed or failed.")
+            break
+
+# Uruchomienie dwóch wątków
+receive_thread = threading.Thread(target=receive)
+send_thread = threading.Thread(target=send)
+
+receive_thread.start()
+send_thread.start()
